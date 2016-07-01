deb199539004c0669f6caee3d9aa077c6c23136d
Esther Leytush <eleytush@gmail.com>
2016-06-30 20:51:51 -0400

    create helper methods for creating shortened links from one and many passed links

--- app/controllers/url_controller.rb
+++ app/controllers/url_controller.rb
@@ -1,10 +1,25 @@
 class UrlController < ApplicationController
+  include UrlHelper
+  
   def shorten
+    render json: shorten_multiple_links
   end
 
-  def url_data
+  def data
+    render json: get_url_data
   end
 
   def redirect_link
+    @short = params[:short]
+    url = Url.find_by(short: @short)
+
+    if url
+      url.click_count += 1
+      url.save
+
+      redirect_to url.body
+    else
+      render "/not_found"
+    end
   end
 end
--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -1,2 +1,32 @@
 module UrlHelper
+  def shorten_multiple_links
+    if params["urls"]
+      json = {"urls": []}
+      params["urls"].each{ |set| json[:urls] << shorten_single_link(set["url"]) }
+      json
+    else
+      shorten_single_link(params["url"])
+    end
+  end
+
+  def shorten_single_link(link)
+    url = Url.find_by(body: link)
+    if url
+      json = {"success": true, "url": link, "short": "http://localhost:3000/#{url.short}"}
+    else
+      url = Url.new(body: link)
+      url.generate_short
+
+      if url.save
+        json = {"success": true, "url": link, "short": "http://localhost:3000/#{url.short}"}
+      else
+        json = {"success": false, "url": link, "errors": url.errors_as_string}
+      end
+    end
+
+    json
+  end
+
+  def get_url_data
+  end
 end
